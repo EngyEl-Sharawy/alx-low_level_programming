c2ccc8b22f7ef0d2321833183e5d4b8db419da07
Engy <engyelsharawy@gmail.com>
2023-06-23 15:45:08 -0700

    let's go

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  *
- * print numbers 1 - 100 followed by a new line
+ * Description: print numbers 1 - 100 followed by a new line
  * numbers that are multiples of 3 print Fizz
  * numbers that are multiples of 5 print Buzz
  * numbers that are multiples of 3 and 5 print FizzBuzz
@@ -22,16 +22,13 @@ int main(void)
 		if (num % 3 == 0 && !(num % 5 == 0))
 		{
 			printf("Fizz");
-		}
-		else if (num % 5 == 0 && !(num % 3 == 0))
+		} else if (num % 5 == 0 && !(num % 3 == 0))
 		{
 			printf("Buzz");
-		}
-		else if (num % 3 == 0 && num % 5 == 0)
+		} else if (num % 3 == 0 && num % 5 == 0)
 		{
 			printf("FizzBuzz");
-		}
-		else
+		} else
 		{
 			printf("%d", num);
 		}
