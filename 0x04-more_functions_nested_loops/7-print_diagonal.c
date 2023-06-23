18a3736b45feddab305eba8783fb085b61589ea5
Engy <engyelsharawy@gmail.com>
2023-06-23 09:32:15 -0700

    let's go

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,7 +17,7 @@ void print_diagonal(int n)
 	{
 		for (postn = 1; postn <= n; postn++)
 		{
-			for (space = 1; space <= postn; space++)
+			for (space = postn; space <= postn; space++)
 			{
 				_putchar(' ');
 			}
